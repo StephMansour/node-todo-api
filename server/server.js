40dd718f8d6af9abb87e927037b972ae09090026
Stephane Mansour <stephane.mansour@gmail.com>
2017-08-07 14:40:10 +0100

    Heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -21,34 +21,32 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
+  }, (e) => {
+    res.status(400).send(e);
   });
-
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
-  }, (err) => {
-    res.status(400).send(err);
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(404).send();
+      return res.status(404).send();
     }
 
     res.send({todo});
-
   }).catch((e) => {
     res.status(400).send();
   });
@@ -66,8 +64,8 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.status(200).send({todo});
-  }).catch((err) => {
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
 });
@@ -82,7 +80,7 @@ app.patch('/todos/:id', (req, res) => {
 
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
-  }else {
+  } else {
     body.completed = false;
     body.completedAt = null;
   }
@@ -92,18 +90,14 @@ app.patch('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.status(200).send({todo});
-
-  }).catch((err) => {
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
-  });
-
-
+  })
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
-
 
 module.exports = {app};
